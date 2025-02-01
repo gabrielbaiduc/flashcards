66b80a961504315f8d9fc555d135b0464af5cc75
Gabriel Baiduc <baiducgabriel@gmail.com>
2025-02-01 12:46:49 +0200

    first commit after reset with SwiftData instead of Core Data and as little overhead as possbile

---
+++ Flashcards/Flashcards/FlashcardsApp.swift
@@ -0,0 +1,32 @@
+//
+//  FlashcardsApp.swift
+//  Flashcards
+//
+//  Created by Gabriel Baiduc on 01/02/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FlashcardsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Flashcards/Flashcards/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Flashcards
+//
+//  Created by Gabriel Baiduc on 01/02/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
