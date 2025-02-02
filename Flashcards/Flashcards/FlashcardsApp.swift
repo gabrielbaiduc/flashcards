ffad8b3b0906bca39d7900b84b8291b355c32d28
Gabriel Baiduc <baiducgabriel@gmail.com>
2025-02-02 09:44:06 +0200

    configure datapoint and views

--- Flashcards/Flashcards/FlashcardsApp.swift
+++ Flashcards/Flashcards/FlashcardsApp.swift
@@ -12,9 +12,9 @@ import SwiftData
 struct FlashcardsApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            Flashcard.self,
         ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: true)
 
         do {
             return try ModelContainer(for: schema, configurations: [modelConfiguration])
@@ -25,7 +25,9 @@ struct FlashcardsApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
+            LearnView()
+            ManageView()
         }
         .modelContainer(sharedModelContainer)
     }
